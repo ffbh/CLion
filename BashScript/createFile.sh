6dd2bcce4ddacf0250d2697de3091825fa7ef7b3
ffbh <2529026882@qq.com>
2018-03-28 08:59:27 +0800

    first submit

---
+++ BashScript/createFile.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+
+BaseDir="/home/fbh/CLionProjects/acm"
+
+
+FileName="  ss  arb asrg     sadf hhh eg    "
+
+
+
+
+FileName=$(echo ${FileName}| awk '{sub("^ *","");sub(" *$","");print}')
+
+
+AllFileName="${FileName}".cpp
+
+
+echo ${FileName} > /home/fbh/jghfdsiergierb.hghageqr
+sed -i "s/[ ][ ]*/\\\ /g" /home/fbh/jghfdsiergierb.hghageqr
+specialFileName=$(cat /home/fbh/jghfdsiergierb.hghageqr).cpp
+rm -f /home/fbh/jghfdsiergierb.hghageqr
+
+
+mv ${BaseDir}/Coding/*    CodeAccept/
+
+cp ${BaseDir}/template/source.cpp   ${BaseDir}/Coding/"${AllFileName}"
+
+echo "" >  ${BaseDir}/InOutput/input.txt
+
+
+
+
+
+echo "" > ${BaseDir}/CMakeLists.txt
+
+CmakeCmd="cmake_minimum_required(VERSION 3.9)
+project(acm)
+set(CMAKE_CXX_STANDARD 11)"
+
+echo "${CmakeCmd}" >> ${BaseDir}/CMakeLists.txt
+
+#add_executable(acm main.cpp)
+
+echo -e "\n" >> ${BaseDir}/CMakeLists.txt
+
+echo -n "add_executable(acm " >> ${BaseDir}/CMakeLists.txt
+
+echo -n "${BaseDir}/Coding/" >> ${BaseDir}/CMakeLists.txt
+
+echo -n "${specialFileName}" >> ${BaseDir}/CMakeLists.txt
+
+echo ")" >> ${BaseDir}/CMakeLists.txt
+
+
+
+
+
+
+
+
+
