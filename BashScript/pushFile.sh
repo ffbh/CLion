aff0d87de1536bfdf7efe5d223d5c822d89cde32
ffbh <2529026882@qq.com>
2018-06-12 20:11:58 +0800

    新增push指定文件

--- BashScript/pushFile.sh
+++ BashScript/pushFile.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 #BaseDir="/home/ffbh/CLionProjects/acm"
 BaseDir=$(dirname $(dirname $(readlink -f ${0})))
-OJ="zoj"
 
-path=/work/code/code/${OJ}/${OJ}/code/
+
+OJ="codeforce"
+
+if [ $# -ge 1 ];then
+	OJ=$1
+fi
+
+
+path=/work/code/${OJ}/${OJ}/code/
 
 echo ${path}
+
 
 mv ${BaseDir}/CodeAccept/*  ${path}
 
